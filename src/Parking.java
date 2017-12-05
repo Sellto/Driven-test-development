469ed0a5aff2af91bed3dda2fdd4fcc1375f94d9
ArmenHagopian <ArmenHagopian@users.noreply.github.com>
2017-12-05 23:18:55 +0100

    Added comments Parking.java

--- src/Parking.java
+++ src/Parking.java
@@ -1,41 +1,40 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-//import java.nio.file.Paths;
-import java.util.stream.Stream;
-//import java.util.Scanner;
-//import java.io.File;
 
 public class Parking extends Observer{
+	//The client receives the code when he buys something in the store
     private String code;
+	//Let the system know if it has to open the gate or not
     private boolean gate;
-    int counter = 0;
+    //Used in checkCode to determine if the attribute "code" is valid
+	int counter = 0;
     private Map<String, Boolean> map = new HashMap<String, Boolean>();
     public Parking()
-	{
+    {
 		map.put("test", true);
 		System.out.println(map.get("test"));
 	}
+    //Check the barcode or QRcode given by the client to the parking system
     public Boolean checkCode()
     {
-
+		//Variable result is true if the barcode is valid
     	Boolean result;
-    	//int counter = 0;
+		
+		//This path depends on the location of the file containing the valid barcodes
+		//The attribute "code" is compared to the valid barcodes contained in the file located in this path
+		//The valid codes are determined and created by the store and written in this file
     	Path path =  FileSystems.getDefault().getPath(".//src//Nouveau.txt");
-    	//System.out.println(FileSystems.getDefault().getPath("") + "test");
-    	//String currentDirectory;
-    	//File file = new File(".");
-    	//currentDirectory = file.getAbsolutePath();
-    	//System.out.println(currentDirectory);
-    	
-    	//Path path =  Paths.get("C:\\Users\\14040\\Desktop\\Nouveau dossier\\src\\Nouveau document texte.txt");
-		// Path path =  Paths.get("C:\\Users\\13022\\Desktop\\TDD lab2\\psw.txt");
+		
+		//Read the .txt file
 		Stream<String> lines = null;
+		
 		try 
 		{
 			
@@ -47,10 +46,10 @@ public class Parking extends Observer{
 			e.printStackTrace();
 		}
 		
-		//lines.forEach(s -> System.out.println(s));
-		
+		//Compare each line to the attribute "code"
 		lines.forEach(s ->  {
-							 if (s.equals(this.code)){
+							//The code is valid
+							if (s.equals(this.code)){
 							  this.counter ++;
 							  //System.out.println("Yes");
 							 }
@@ -61,7 +60,7 @@ public class Parking extends Observer{
 							}
 		);
 		
-    	
+    	//The code is valid as the same exists in the file containing the valid codes
     	if(this.counter == 1)
     	{
     		result = true;
@@ -73,10 +72,13 @@ public class Parking extends Observer{
     	
     	return result;
     }
+	
     public void setCode(String code)
     {
     	this.code = code;
     }
+	
+	//Determines and returns the number of free spaces to park a car
     public int numberFreeSpaces()
     {
     	int freeSpaces = 0;
@@ -88,7 +90,7 @@ public class Parking extends Observer{
     	for(int i=0; i<size; i++)
     	{
     		key = (String) keyArray[i];
-    		
+    		//Get the value corresponding to each parking spot to know if it is free or not
     		testBool = this.map.get(key);
     		if (testBool == false)
     		{
@@ -98,13 +100,22 @@ public class Parking extends Observer{
 
     	return freeSpaces;
     }
+	
+	//Update the dictionary containing the state of each parking spot
     public void update(Map<String, Boolean> newValues)
     {
     	this.map = newValues;
     }
+	
+	//Used when the code attribute has been verified and is valid
     public void openGate()
     {
-    		gate = true;
+    	this.gate = true;
+    }
+	//Used after a car passed the gate
+	public void closeGate()
+    {
+    	this.gate = false;
     }
 }
   
